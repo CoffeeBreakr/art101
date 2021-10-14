513637c5ec9fe9cb84ecd8d5260d37e17df5d78e
CoffeeBreakr <jesapark@ucsc.edu>
2021-10-14 01:55:17 -0700

    Lab 4 Task 1,2,3

---
+++ lab4/js/lab.js
@@ -0,0 +1,19 @@
+/**
+ * Summary. Lab 4 - Pseudo Code.
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Jesse Park & Evan Kramer
+ * @since  x.x.x
+ */
+
+ // Creating a Dino Run game:
+ // 1. Draw the game, parallax background, ground, dinosaur.
+ // 2. Get user input.
+ // 3. Start the game and begin a timer.
+ // 4. Change the horizontal speed of the floor and parallax background.
+ // 5. Occasionally add in obstacles based on the value of the timer and check for user input.
+ // 6. Keep repeating step 5 until player dies.
+ // 7. Restart.
