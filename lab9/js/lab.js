c2f4ccf771c647cc0d00d3f9d09cc76cf68a00de
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-04 18:04:24 -0800

    Thanks Andrew

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,9 +6,26 @@
 
 // Javascript for Web
 
-oldEl = document.getElementById('kapow');
-newEl = document.createElement("div");
-oldEl.appendChild();
-newEl.id = "kaboom";
-newEl.innerHTML = "KABOOOM!";
-newEl.style.fontSize = "30pt";
+//Task 2
+var oldEl = document.getElementById('output');
+//create a var that gets the info with the ID fo "output"
+var new1El = document.createElement("p");
+new1El.innerHTML = "<p>Something New</p>"";
+
+//create a variable and modifyt its html to be a paragraph
+
+var new2El = document.createElement("p");
+new2El.innerHTML = "<p>Something Newer<p>";
+
+//create a variable and modift its html to be a paragraph
+
+oldEl.appendChild(new1El);
+oldEl.appendChild(new2El);
+
+//added new1El and new2El into oldEl
+
+newEl1.style.color = "red";
+newEl1.style.fontSize = "30pt";
+newEl2.style.color = "blue";
+newEl2.style.fontSize = "15";
+oldEl.style.outline = "thin solid black"
