b514c57da05920e5201bdac6e197eef09b64beed
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-05 00:04:41 -0800

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,15 +1,51 @@
 /**
  * Author:    Jesse Park & Evan Kramer
- * Created:   10/26/21
+ * Created:   12/4/21
  *
  **/
+ var oldEl = document.getElementById('output');
 
-// Defining Variables
-function DoStuff(x){
-   var results = x * 3
-   return results;
+
+
+function FizzBuzzBoom(){
+
+  var outputStr = "";
+  var num;
+  //iterate over all out numbers
+  for(let i = 0; i <=200; i++){
+          var outputStr = "";
+          num = i;
+    //is the number a multiple of 3?
+      if(i % 3 == 0){
+        outputStr += "Fizz!";
+      }
+    //is the number a multiple of 5?
+      if(i % 5 == 0){
+        outputStr += "Buzz!";
+      }
+    //is the number a multiple of 7?
+      if(i % 7 == 0){
+        outputStr += "Boom!";
+      }
+
+
+if(outputStr != ""){
+      $("#output").append("<p>" + num + " - " + outputStr + "</p>");
 }
 
- //output
-document.writeln("Here is your new name: ", sortUserName(), "</br>");
-console.log(DoStuff(3));
+
+  }
+
+  return outputStr;
+}
+
+document.getElementById("btn").addEventListener("click", function() {
+
+FizzBuzzBoom();
+
+});
+
+
+function reportError(str) {
+  outputEl.innerHTML = "<div> class = 'error'" + str + "</div>"
+}
