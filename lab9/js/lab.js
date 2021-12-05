f120f96cfcc23d7ea73377ea872ddd8912cad520
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-04 18:29:50 -0800

    changes

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,26 +6,25 @@
 
 // Javascript for Web
 
-//Task 2
 var oldEl = document.getElementById('output');
 //create a var that gets the info with the ID fo "output"
-var new1El = document.createElement("p");
-new1El.innerHTML = "<p>Something New</p>"";
+var new1El = document.createElement("p2");
+new1El.innerHTML = "<p2>Something New</p2>";
 
 //create a variable and modifyt its html to be a paragraph
 
-var new2El = document.createElement("p");
-new2El.innerHTML = "<p>Something Newer<p>";
+var new2El = document.createElement("p1");
+new2El.innerHTML = "<p1>Something Newer than that other element next to me<p1>";
 
-//create a variable and modift its html to be a paragraph
+//create a variable and modify its html to be a paragraph
 
 oldEl.appendChild(new1El);
 oldEl.appendChild(new2El);
 
 //added new1El and new2El into oldEl
 
-newEl1.style.color = "red";
-newEl1.style.fontSize = "30pt";
-newEl2.style.color = "blue";
-newEl2.style.fontSize = "15";
-oldEl.style.outline = "thin solid black"
+new1El.style.color = "red";
+new1El.style.fontSize = "30pt";
+new2El.style.color = "blue";
+new2El.style.fontSize = "15pt";
+oldEl.style.outline = "thick groove #FFFFFF";
