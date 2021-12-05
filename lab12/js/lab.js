5b1091871fb6e453515b14d188529b62704ae59e
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-04 22:17:55 -0800

    lab 12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,15 +1,48 @@
 /**
  * Author:    Jesse Park & Evan Kramer
- * Created:   10/26/21
+ * Created:   12/4/21
  *
  **/
 
-// Defining Variables
-function DoStuff(x){
-   var results = x * 3
-   return results;
+var house;
+var oldEl = document.getElementById('output');
+var new1El = document.createElement("p3");
+
+new1El.style.color = "yellow";
+new1El.style.fontSize = "15pt";
+
+//sortingHat function
+function sortingHat(str){
+  var length = str.length;
+  var mod = length % 4;
+  console.log(mod);
+
+  if(mod == 0){
+    return "Gryffindor";
+    console.log("returned Gryf");
+  }
+  else if(mod == 1){
+    return "Ravenclaw";
+        console.log("returned Raven");
+  }
+  else if(mod == 2){
+    return "Slytherin";
+        console.log("returned Snake");
+  }
+  else if(mod == 3){
+    return "Hufflepuff";
+        console.log("returned Huffle");
+  }
 }
 
- //output
-document.writeln("Here is your new name: ", sortUserName(), "</br>");
-console.log(DoStuff(3));
+//button function
+
+document.getElementById("btn").addEventListener('click', function(){
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  console.log(name);
+  console.log(house);
+  var outPut = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  new1El.innerHTML = outPut;
+  oldEl.appendChild(new1El);
+});
