ad70fac74ea07569e5ba5f93144ff6dfa9c4a9e6
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-04 20:05:23 -0800

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,15 +1,39 @@
 /**
  * Author:    Jesse Park & Evan Kramer
- * Created:   10/26/21
+ * Created:   12/4/21
  *
  **/
 
-// Defining Variables
-function DoStuff(x){
-   var results = x * 3
-   return results;
+ //div
+ var oldEl = document.getElementById('output');
+ var new1El = document.createElement("p");
+
+ //Style
+  new1El.style.color = "white";
+  new1El.style.fontSize = "12pt";
+  new1El.style.border = "thin groove #FFFFFF"
+
+//function to grab an inputed name and sort it.
+ function sortUserName(){
+   var userName = window.prompt("Enter your username.");
+   console.log("userName =", userName);
+   //string to array
+   var nameArray = userName.split('');
+   console.log("nameArray =", nameArray);
+   //sort
+   var nameArraySort = nameArray.sort();
+   console.log("nameArraySort =", nameArraySort);
+   //rejoin aray with string
+   var nameSorted = nameArraySort.join('');
+   console.log("nameSorted =", nameSorted);
+   return nameSorted;
 }
 
- //output
-document.writeln("Here is your new name: ", sortUserName(), "</br>");
-console.log(DoStuff(3));
+//button function
+   document.getElementById("btn").addEventListener('click', function(){
+     console.log("Button has been clicked!");
+     new1El.innerHTML = sortUserName();
+     console.log("El1 has been created");
+     oldEl.appendChild(new1El);
+     console.log("El1 has been added");
+   });
