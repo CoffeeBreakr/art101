f356dca6a22b5d265a4f175086c89ee79168e272
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-05 11:47:55 -0800

    15 lab done

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -4,32 +4,35 @@
  *
  **/
 
+//var myURL ="https://numbersapi.com/42/math";
+var myURL = "https://api.kanye.rest/"
 var oldEl = document.getElementById('output');
 
+$("#activate").click(callAJAX);
+
 function callAJAX() {
+  console.log("button has been pressed")
   // Using the core $.ajax() method
    $.ajax({
        // API endpoint
-       url: "https://numbers.api",
+       url: myURL,
        // Any data to send
-       data: { id: 123},
+       //data: { id: 123},
        // POST or GET request
        type: "GET",
        // data type we expect back
-       dataType : "json",
+       //dataType : "json",
    })
 
    // If the request succeeds
    // data is passed back
    .done(function(data) {
        console.log("Success:", data);
+       $("#output").html(data.quote);
    })
    // If the request fails
    .fail(function(request,error) {
        console.log(request, error);
+       $("#output").html("Error")
    });
   }
-
-document.getElementById("activate").addEventListener("click", function() {
-callAJAX();
-});
