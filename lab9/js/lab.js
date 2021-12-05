b029a1cd53bb0eff987634f0dfa9fd00208ac50a
CoffeeBreakr <jesapark@ucsc.edu>
2021-12-04 18:56:18 -0800

    test 2

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,18 +8,22 @@
 
 var oldEl = document.getElementById('output');
 //create a var that gets the info with the ID fo "output"
-var new1El = document.createElement("p2");
-new1El.innerHTML = "<p2>Something New</p2>";
+var new1El = document.createElement("p3");
+new1El.innerHTML = "<p3>Something New</p3>";
+console.log("El1 has been created");
 
 //create a variable and modifyt its html to be a paragraph
 
-var new2El = document.createElement("p1");
-new2El.innerHTML = "<p1>Something Newer than that other element next to me<p1>";
+var new2El = document.createElement("p3");
+new2El.innerHTML = "<p3>Something Newer than that other element next to me<p3>";
+console.log("El2 has been created");
 
 //create a variable and modify its html to be a paragraph
 
 oldEl.appendChild(new1El);
+console.log("El1 has been added");
 oldEl.appendChild(new2El);
+console.log("El2 has been added");
 
 //added new1El and new2El into oldEl
 
